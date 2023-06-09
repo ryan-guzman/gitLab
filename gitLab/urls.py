7939d87c959baec196b38937eacf93fc42e8e058
ryan-guzman <ryankguzman@gmail.com>
2023-06-09 09:55:12 -0700

    no

--- gitLab/urls.py
+++ gitLab/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from git_test import views as git_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', git_views.home)
 ]
